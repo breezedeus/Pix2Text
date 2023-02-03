d54a9b8ac7fa697161c325e000bb2dd10ba3b4f1
breezedeus <breezedeus@163.com>
2023-02-03 13:25:27 +0800

    refactor

--- scripts/screenshot_daemon.py
+++ scripts/screenshot_daemon.py
@@ -11,6 +11,7 @@ import glob
 import pyperclip as pc
 
 from pix2text import set_logger, Pix2Text, render_html
+
 logger = set_logger(log_level='DEBUG')
 
 SCREENSHOT_DIR = os.getenv(
@@ -21,7 +22,8 @@ thresholds = {
     'formula2general': 0.65,  # 如果识别为 `formula` 类型，但得分小于此阈值，则改为 `general` 类型
     'english2general': 0.75,  # 如果识别为 `english` 类型，但得分小于此阈值，则改为 `general` 类型
 }
-P2T = Pix2Text(thresholds=thresholds)
+config = dict(layout=dict(model_name='mfd'), thresholds=thresholds)
+P2T = Pix2Text.from_config(config)
 
 
 def get_newest_fp_time(screenshot_dir):
@@ -45,29 +47,27 @@ def recognize(screenshot_dir, delta_interval):
             logger.info('image type: %s, image text: %s', image_type, result)
             if result:
                 pc.copy(result)
-            render_html('./analysis_res.jpg', image_type, result, out_html_fp='out-text.html')
+            # render_html('./analysis_res.jpg', image_type, result, out_html_fp='out-text.html')
         time.sleep(1)
 
 
 def _recognize_newest(newest_fp):
-    res = P2T.recognize(newest_fp, use_layout=False, save_analysis_res='./analysis_res.jpg')
+    res = P2T.recognize(
+        newest_fp,
+        use_analyzer=True,
+        save_analysis_res='./analysis_res.jpg',
+        embed_sep=(' $$', '$$ '),
+        isolated_sep=('\n', '\n'),
+    )
     if len(res) == 1:
         return res[0]['type'], res[0]['text']
     elif len(res) > 1:
         box_types = set([info['type'] for info in res])
         if len(box_types) > 1:
             image_type = 'hybrid'
-            if any([info['type'] == 'formula' for info in res]):
-                text = []
-                for info in res:
-                    if info['type'] == 'formula':
-                        text.append(info['text'])
-                    else:
-                        text.append(r'\text{' + info['text'] + r'}')
-                text = '\n\n'.join(text)
         else:
             image_type = list(box_types)[0]
-            text = '\n\n'.join([info['text'] for info in res])
+        text = '\n'.join([info['text'] for info in res])
 
         return image_type, text
 
