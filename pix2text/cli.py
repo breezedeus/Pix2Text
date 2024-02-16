e943ac31268111652c0e6987b824f95ce692e57b
breezedeus <breezedeus@163.com>
2024-02-16 10:10:03 +0800

    update consts

--- pix2text/cli.py
+++ pix2text/cli.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# [Pix2Text](https://github.com/breezedeus/pix2text): an Open-Source Alternative to Mathpix.
 # Copyright (C) 2022-2024, [Breezedeus](https://www.breezedeus.com).
 
 import os
