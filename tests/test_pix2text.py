e55d8c99f4f574298824e1743aa43bb72be4a201
breezedeus <breezedeus@163.com>
2024-02-22 00:08:40 +0800

    more tests

--- tests/test_pix2text.py
+++ tests/test_pix2text.py
@@ -15,13 +15,15 @@ def test_mfd():
 
 
 def test_example():
-    img_fp = './docs/examples/formula.jpg'
+    # img_fp = './docs/examples/formula.jpg'
+    img_fp = './docs/examples/mixed.jpg'
     formula_config = {
         # 'model_name': 'mfr-pro',
         # 'model_backend': 'onnx',
     }
     p2t = Pix2Text(formula_config=formula_config)
-    print(p2t.recognize_formula(img_fp))
+    print(p2t.recognize(img_fp))
+    # print(p2t.recognize_formula(img_fp))
     # outs = p2t(img_fp, resized_shape=608, save_analysis_res='./analysis_res.jpg')  # can also use `p2t.recognize(img_fp)`
     # print(outs)
     # # To get just the text contents, use:
@@ -29,17 +31,29 @@ def test_example():
     # print(only_text)
 
 
-# def test_post_post_process():
-#     latex = r'\log(p(x\mid q(x)))+||\mathrm{sg}\left[z_{e}(x)\right]-e||_{2}^{2}+\beta\left||z_{e}(x)-\mathrm{sg}[e]|_{2}^{2}'  # noqa: E501
-#     out = post_post_process_latex(latex)
-#     print(f'{latex}\n->\n{out}\n')
-#     # 以下是其他的测试字符串来测试此接口功能是否符合预期
-#     latex = r'(a+b)^2 +（a-b）^2'
-#     out = post_post_process_latex(latex)
-#     print(f'{latex}\n->\n{out}\n')
-#
-#     # 下面这个Latex是正确的，不应该被修改
-#     latex = r'\left\{\begin{array}{ll}1 & \text { if } x>0 \\0 & \text { if } x \leq 0\end{array}\right.'
-#     out = post_post_process_latex(latex)
-#     assert out == latex
-#     print(f'{latex}\n==\n{out}\n')
+def test_example_mixed():
+    img_fp = './docs/examples/en1.jpg'
+    p2t = Pix2Text()
+    outs = p2t.recognize(img_fp, resized_shape=608)  # 也可以使用 `p2t(img_fp)` 获得相同的结果
+    print(outs)
+    # 如果只需要识别出的文字和Latex表示，可以使用下面行的代码合并所有结果
+    only_text = merge_line_texts(outs, auto_line_break=True)
+    print(only_text)
+
+
+def test_example_formula():
+    from pix2text import Pix2Text
+
+    img_fp = './docs/examples/math-formula-42.png'
+    p2t = Pix2Text()
+    outs = p2t.recognize_formula(img_fp)
+    print(outs)
+
+
+def test_example_text():
+    from pix2text import Pix2Text
+
+    img_fp = './docs/examples/general.jpg'
+    p2t = Pix2Text()
+    outs = p2t.recognize_text(img_fp)
+    print(outs)
