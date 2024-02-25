36985c3760200aaf74873f2e4cc43aff7cbe654e
breezedeus <breezedeus@163.com>
2024-02-25 09:39:17 +0800

    more tests

--- tests/test_pix2text.py
+++ tests/test_pix2text.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
+import os
+
 from pix2text import Pix2Text, merge_line_texts
-# from pix2text.latex_ocr import post_post_process_latex
 
 
 def test_mfd():
@@ -18,8 +19,8 @@ def test_example():
     # img_fp = './docs/examples/formula.jpg'
     img_fp = './docs/examples/mixed.jpg'
     formula_config = {
-        # 'model_name': 'mfr-pro',
-        # 'model_backend': 'onnx',
+        'model_name': 'mfr-pro',
+        'model_backend': 'onnx',
     }
     p2t = Pix2Text(formula_config=formula_config)
     print(p2t.recognize(img_fp))
@@ -29,6 +30,66 @@ def test_example():
     # # To get just the text contents, use:
     # only_text = merge_line_texts(outs, auto_line_break=True)
     # print(only_text)
+
+
+def test_blog_example():
+    img_fp = './docs/examples/mixed.jpg'
+
+    p2t = Pix2Text(
+        analyzer_config=dict(  # 声明 LayoutAnalyzer 的初始化参数
+            model_name='mfd',
+            model_type='yolov7',  # 表示使用的是 YoloV7 模型，而不是 YoloV7_Tiny 模型
+            model_fp=os.path.expanduser(
+                '~/.cnstd/1.2/analysis/mfd-yolov7-epoch224-20230613.pt'
+            ),  # 注：修改成你的模型文件所存储的路径
+        ),
+        formula_config=dict(
+            model_name='mfr-pro',
+            model_backend='onnx',
+            model_dir=os.path.expanduser(
+                '~/.pix2text/1.0/mfr-pro-onnx'
+            ),  # 注：修改成你的模型文件所存储的路径
+        ),
+    )
+    outs = p2t.recognize(img_fp, resized_shape=608)  # 也可以使用 `p2t(img_fp)` 获得相同的结果
+    print(outs)
+    # 如果只需要识别出的文字和Latex表示，可以使用下面行的代码合并所有结果
+    only_text = merge_line_texts(outs, auto_line_break=True)
+    print(only_text)
+
+
+def test_blog_pro_example():
+    img_fp = './docs/examples/mixed.jpg'
+
+    p2t = Pix2Text(
+        languages=('en', 'ch_sim'),
+        analyzer_config=dict(  # 声明 LayoutAnalyzer 的初始化参数
+            model_name='mfd',
+            model_type='yolov7',  # 表示使用的是 YoloV7 模型，而不是 YoloV7_Tiny 模型
+            model_fp=os.path.expanduser(
+                '~/.cnstd/1.2/analysis/mfd-yolov7-epoch224-20230613.pt'
+            ),  # 注：修改成你的模型文件所存储的路径
+        ),
+        formula_config=dict(
+            model_name='mfr-pro',
+            model_backend='onnx',
+            model_dir=os.path.expanduser(
+                '~/.pix2text/1.0/mfr-pro-onnx'
+            ),  # 注：修改成你的模型文件所存储的路径
+        ),
+        text_config=dict(
+            rec_model_name='doc-densenet_lite_666-gru_large',
+            rec_model_backend='onnx',
+            rec_model_fp=os.path.expanduser(
+                '~/.cnocr/2.3/doc-densenet_lite_666-gru_large/cnocr-v2.3-doc-densenet_lite_666-gru_large-epoch=005-ft-model.onnx'  # noqa
+            ),  # 注：修改成你的模型文件所存储的路径
+        ),
+    )
+    outs = p2t.recognize(img_fp, resized_shape=608)  # 也可以使用 `p2t(img_fp)` 获得相同的结果
+    print(outs)
+    # 如果只需要识别出的文字和Latex表示，可以使用下面行的代码合并所有结果
+    only_text = merge_line_texts(outs, auto_line_break=True)
+    print(only_text)
 
 
 def test_example_mixed():
